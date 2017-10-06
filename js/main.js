7e36e2096ad753666502f6a9ac35c2ad01b9ea4c
belocer <belocerkovecden@gmail.com>
2017-10-07 00:01:19 +0300

    start

--- js/main.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function(){
-    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-    particlesJS.load('particles-js', 'json/particles.json', function() {
-        //console.log('callback - particles.js config loaded');
-    });
-});
